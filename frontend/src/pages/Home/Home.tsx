1079ddf9dafd36eab3e91f718e50364bbb4892cf
Fernando Warno <ferwar@hotmail.com>
2024-01-04 19:52:17 -0300

    customizacion del hook

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useCustomSelector, useCustomDispatch } from 'hooks/redux';
+import { setAccessToken } from 'redux/slices/auth';
 
 const Home: React.FC = () => {
-  const { auth } = useSelector((state: any) => state);
+  const { auth } = useCustomSelector((state) => state);
+  // const auth = useCustomSelector((state) => state.auth);
+  // const accessToken = useCustomSelector((state) => state.auth.accessToken);
+  const dispatch = useCustomDispatch();
 
   console.log(auth);
+  // console.log(accessToken);
 
-  return <div>Home</div>;
+  function handleLogin(): void {
+    dispatch(setAccessToken('tokenquevoyamandar'));
+  }
+
+  return (
+    <div>
+      Home
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 };
 
 export default Home;
